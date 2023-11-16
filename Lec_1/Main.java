f5804a1f304177cde60d0df5c37e4597f69fcde2
prashantkharche <prashantkharche2002@gmail.com>
2023-11-16 23:36:05 +0530

    Basics

---
+++ Lec_1/Main.java
@@ -0,0 +1,12 @@
+package Lec_1;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("What is your age :");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Your age is " + input.nextInt());
+    }
+}
