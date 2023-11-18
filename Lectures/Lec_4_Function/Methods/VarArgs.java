e299ac85fe5802c7ed395617789d22b977af6639
prashantkharche <prashantkharche2002@gmail.com>
2023-11-18 21:03:59 +0530

    VarArgs

---
+++ Lectures/Lec_4_Function/Methods/VarArgs.java
@@ -0,0 +1,19 @@
+package Lec_4;
+
+import java.util.Arrays;
+
+public class VarArgs {
+    public static void main(String[] args) {
+        fun(1,2,3,4,5,6,7);
+        multiple_arg(1, "Prashant", 'a','b','c','d','f','r','g');
+    }
+
+
+    static void multiple_arg(int a,String b,char ...c){
+        System.out.println(a + " " + b +" "+ Arrays.toString(c));
+    }
+
+    static void fun(int ...a){
+        System.out.println(Arrays.toString(a));
+    }
+}
