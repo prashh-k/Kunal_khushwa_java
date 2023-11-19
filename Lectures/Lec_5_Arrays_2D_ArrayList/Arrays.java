6fd3ac562b2dec09a21df8c859c77eae53e8c634
prashantkharche <prashantkharche2002@gmail.com>
2023-11-20 01:22:53 +0530

    Arrays

---
+++ Lectures/Lec_5_Arrays_2D_ArrayList/Arrays.java
@@ -0,0 +1,30 @@
+package Lec_5;
+
+public class Arrays {
+    public static void main(String[] args) {
+        // Syntax :
+//        datatype[] variable_name = new datatype[size];
+
+        //store rollnos of 5 student
+        int[] rollnos = new int[5];
+        //OR
+        int[] rollno = {1,2,3,4,5};
+
+
+        int[] nos ;             // declaration of array nos is defined in stack.
+        nos = new int[5];       // initialization of array nos : here object is being created in memory (Heap)
+
+
+        //        System.out.println(ros[1]);
+
+        String[] arr = new String[4];
+        System.out.println(arr[0]);
+
+//        for (String element : arr) {
+//            System.out.println(element);
+//        }
+
+
+
+    }
+}
