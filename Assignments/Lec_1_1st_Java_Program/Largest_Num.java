fa9838c40c02d0fc88f8193e0cda784f09d3cb28
prashantkharche <prashantkharche2002@gmail.com>
2023-11-28 00:04:49 +0530

    greater num

---
+++ Assignments/Lec_1_1st_Java_Program/Largest_Num.java
@@ -0,0 +1,20 @@
+package Assignments.Lec_1_1st_Java_Program;
+
+import java.util.Scanner;
+
+public class Largest_Num {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        if (a < b) {
+            System.out.println(b +" is a larger number.");
+        } else if (a == b) {
+            System.out.println("Both numbers are same");
+        } else {
+            System.out.println(a + " is a larger number.");
+        }
+    }
+}
