933b25fac6cad29c8489533a9ff806d39960c64d
prashantkharche <prashantkharche2002@gmail.com>
2023-11-29 22:38:37 +0530

    Question-Currency

---
+++ Assignments/Lec_1_1st_Java_Program/Currency_IND_USD.java
@@ -0,0 +1,22 @@
+package Assignments.Lec_1_1st_Java_Program;
+
+import java.util.Scanner;
+
+public class Currency_IND_USD {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("If IND to USD type IU else type UI :");
+        String CurrencyChange = in.next();
+        System.out.println("Enter Value: ");
+        float Val = in.nextFloat();
+
+        if (CurrencyChange.equals("IU")){
+            Val = Val / 83 ;
+        } else if (CurrencyChange.equals("UI")){
+            Val = Val * 83 ;
+        }
+        System.out.println(Val);
+
+    }
+}
