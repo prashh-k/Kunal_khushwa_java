cff4b5fb7832dcea71e4c99caec51b20eb2d32b3
prashantkharche <prashantkharche2002@gmail.com>
2023-11-30 20:53:52 +0530

    Questtion

---
+++ Assignments/Lec_2_conditional_loop/Area_Rectangle.java
@@ -0,0 +1,15 @@
+package Assignments.Lec_2_conditional_loop;
+
+import java.util.Scanner;
+
+public class Area_Rectangle {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int Length = in.nextInt();
+        int Width = in.nextInt();
+
+        int Area = Length * Width;
+        System.out.println(Area);
+    }
+}
