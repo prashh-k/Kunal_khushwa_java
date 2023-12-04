b517031d68ca47a508e53b78e461b603e93858bb
prashantkharche <prashantkharche2002@gmail.com>
2023-12-05 00:06:08 +0530

    perimeter Rectangle

---
+++ Assignments/Lec_2_conditional_loop/Perimeter_Rectangle.java
@@ -0,0 +1,15 @@
+package Assignments.Lec_2_conditional_loop;
+
+import java.util.Scanner;
+
+public class Perimeter_Rectangle {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        double length = in.nextDouble();
+        double width = in.nextDouble();
+
+        double Perimeter = 2*( length + width );
+        System.out.println(Perimeter);
+    }
+}
