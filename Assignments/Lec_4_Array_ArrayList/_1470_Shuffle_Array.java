8c09f2c071c197c47f9e9575e363bfed53e09635
prashantkharche <prashantkharche2002@gmail.com>
2023-12-13 01:15:29 +0530

    Shuffle array

---
+++ Assignments/Lec_4_Array_ArrayList/_1470_Shuffle_Array.java
@@ -0,0 +1,23 @@
+package Assignments.Lec_4_Array_ArrayList;
+
+import java.util.Arrays;
+
+public class _1470_Shuffle_Array {
+    public static void main(String[] args) {
+        int[] arr = {2,5,1,3,4,7};
+
+        System.out.println(Arrays.toString(shuffle(arr)));
+    }
+
+    public static int[] shuffle(int[] nums){
+        int[] ans = new int[nums.length];
+        int ni = nums.length/2 ;
+
+        for(int i =0; i < ni;i++){
+
+                ans[2*i] = nums[i] ;
+            ans[2*i + 1] = nums[ni+i] ;
+        }
+    return ans;
+    }
+}
